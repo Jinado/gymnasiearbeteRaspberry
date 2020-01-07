868c1dfe07f31932c798126cd4f2c980c9c3b7d8
Johannes Emmoth <johannes.emmoth@gmail.com>
2020-01-07 17:38:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# USE THE FOLLOWING COMMAND TO RUN THE SCRIPT EVERY THIRTY SECONDS
+`while true; do ./checkForUpdatedString.py; sleep 30; done`
---
+++ checkForUpdatedString.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import codecs
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(host='localhost', database='gymnasiearbete', user='garasp', password='Timtaga.Ihyli!Ctwba.')
+
+    sql_query = "SELECT string FROM raspberries WHERE email LIKE 'johannes.emmoth@gmail.com' AND name LIKE 'Kontorsdörr 1'"
+    cursor = connection.cursor()
+    cursor.execute(sql_query)
+    records = cursor.fetchall()
+
+    # Check to see if a file exists, if it does, open it and read the first line
+    # then compare the read value with the fetched value, if they're equal, do nothing
+    # if they're not equal, write the fetched value into the file and then close it
+    # print the fetched value
+
+    try:
+        readVal = ""
+        with codecs.open('./screenData.txt', 'r', 'utf-8-sig') as f:
+            readVal = f.readlines()
+        if(readVal[0] != "" and readVal[0] != records[0][0]):
+            # NOT THE SAME
+            with codecs.open('./screenData.txt', 'w', 'utf-8-sig') as f:
+                f.write(records[0][0])
+
+            print "======================="
+            print records[0][0]
+            print "======================="
+    except IOError:
+        with codecs.open('./screenData.txt', 'w', 'utf-8-sig') as f:
+            f.write(records[0][0])
+    
+        print "=========================="
+        print records[0][0]
+        print "=========================="
+ 
+    # If the file does not exist, create it and write the fetched value to it.
+    # After that, print the fetched value
+
+except Error as e:
+    print "Error reading data from MySQL table", e
+finally:
+    if(connection.is_connected()):
+        connection.close()
+        cursor.close()
